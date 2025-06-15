346db5e70e7c6827dcac68a84342d085cd4d73bd
Nick <ex-er-sis@ya.ru>
2025-06-15 05:11:35 +0300

    Remove unnecs

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { componentTagger } from "lovable-tagger";
 
+import { version } from './package.json';
+
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    esbuild: {
+      pure: mode === 'production' ? ['console.log'] : [],
     },
-  },
-}));
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    // logLevel: mode === 'production' ? 'silent' : 'warn',
+    // clearScreen: mode === 'production',
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      mode === 'development' && componentTagger()
+    ].filter(Boolean),
+    build: {
+      assetsInlineLimit: 1024, // Ресурсы меньшего размера будут встроены как base64
+      sourcemap: false,
+    },
+    define: {
+      __APP_VERSION__: JSON.stringify(version),
+    },
+
+  }
+})
